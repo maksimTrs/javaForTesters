c719e0c363efd80c710bcd0215c100917518b9a1
Maksim T <roronoazorroippo@gmail.com>
2023-01-11 15:58:51 +0400

    refactoring 4

--- src/test/java/utils/SecretGetter.java
+++ src/test/java/utils/SecretGetter.java
@@ -2,18 +2,22 @@ package utils;
 
 import org.bouncycastle.util.encoders.Base64;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 public class SecretGetter {
 
-   // private static final String PASSWORD_BASE64_DATA = "secret";
+    // private static final String PASSWORD_BASE64_DATA = "secret";
 
-    public static String returnPass(String pass)  {
+    private static String returnPass(String pass) {
         if (pass != null && pass.equals("c2VjcmV0")) {
+            System.out.println("*** Password validated without errors ***");
             return new String(Base64.decode(pass.getBytes(StandardCharsets.UTF_8)));
         }
-        throw new IllegalArgumentException("Add correct password!");
+        throw new IllegalArgumentException("***** ERROR: Add correct password!!! *****");
+    }
+
+    public static String handlingPassword(String pass) {
+        System.out.println("*** Handle Base64 password ***");
+        return returnPass(pass);
     }
 }
